64a87073c45365b8148e07224e6e6813f336d413
Andrea <andreademarco91@gmail.com>
2020-02-03 15:52:14 +0100

    UPUP

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
 // CREO CICLO PER INSERIRE GIORNI
   for (var i = 1; i <= month_days; i++) {
-    var day = '2018' + ' - ' + addZero(month) +  ' - ' + addZero(i);
+    var day = '2018' + '-' + addZero(month) +  '-' + addZero(i);
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
     var context = {
@@ -22,15 +22,22 @@ $.ajax({
   url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
   method: "GET",
   success: function (data, stato) {
-    var response = data.response;
-        console.log(response);
+    var holiday = data.response;
+    console.log(holiday);
+    for (var i = 0; i < holiday.length; i++) {
+      console.log(holiday[i].date);
+      $('li').each(function() {
+        var dayAttr = $(this).attr('data-holi');
+        if (dayAttr == holiday[i].date) {
+          $(this).addClass('red');
+        }
+      });
+    }
   },
   error: function (richiesta, stato, errori) {
-  alert("E' avvenuto un errore. " + errore);
+    alert("E' avvenuto un errore. " + errore);
   }
 });
-
-
 // ADDZERO
 function addZero(num) {
   if (num < 10) {
