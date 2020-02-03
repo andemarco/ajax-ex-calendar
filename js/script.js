5a5eed8e75a0007ba81e0e94166133b07f2b6314
Andrea <andreademarco91@gmail.com>
2020-02-03 15:29:18 +0100

    DATE UP

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // DEFINISCO IL MOMENTO
 $(document).ready(function(){
-  var month = 0;
-  var month_days = moment('2018-(mounth+1)', "YYYY-MM").daysInMonth();
+  var month = 1;
+  var month_days = moment('2018-(month)', "YYYY-MM").daysInMonth();
 
 
 // CREO CICLO PER INSERIRE GIORNI
   for (var i = 1; i <= month_days; i++) {
-    var day = i;
+    var day = '2018' + ' - ' + addZero(month) +  ' - ' + addZero(i);
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
     var context = {
@@ -16,3 +16,27 @@ $(document).ready(function(){
     $('.month_days').append(html);
   }
 });
+
+// CHIAMO AJAX PER FESTIVITA'
+$.ajax({
+  url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+  method: "GET",
+  success: function (data, stato) {
+    var response = data.response;
+        console.log(response);
+  },
+  error: function (richiesta, stato, errori) {
+  alert("E' avvenuto un errore. " + errore);
+  }
+});
+
+
+// ADDZERO
+function addZero(num) {
+  if (num < 10) {
+    return "0"+num
+  }
+  else {
+    return num
+  }
+}
